bc84dc6619ec47e0347f7ee19ae00a0b14fc15bd
Stephan Muller <stephan@frontmen.nl>
2016-07-08 15:34:03 +0200

    Forgot to build min version

--- dist/main.js
+++ dist/main.js
@@ -9,11 +9,13 @@ const STEPS = [
     [ 1,  0],
     [ 1,  1]
 ];
+const GENERATION_TIMEOUT = 25;
 
 let tiles = new Map();
 let stopped = false;
 let timeout;
 let onStopCallback;
+
 
 module.exports = class {
 
@@ -42,11 +44,11 @@ module.exports = class {
 
         this.drawGeneration();
 
-        timeout = setTimeout(this.live.bind(this), 1000);
+        timeout = setTimeout(this.live.bind(this), GENERATION_TIMEOUT);
     }
 
     reset() {
-        let pattern = this.firstGeneration.map(this.toIndex.bind(this));;
+        let pattern = this.firstGeneration.map(this.toIndex.bind(this));
 
         tiles.forEach((tile) => {
             tile.alive = false;
@@ -85,7 +87,7 @@ module.exports = class {
             onStopCallback();
 
         } else {
-            timeout = setTimeout(this.live.bind(this), 250);
+            timeout = setTimeout(this.live.bind(this), GENERATION_TIMEOUT);
         }
     }
 
